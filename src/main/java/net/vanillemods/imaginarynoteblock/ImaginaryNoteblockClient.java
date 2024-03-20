1342dbf74bf68998bdab13c7fabfebc103dcd97a
vanilleuwu <schatatzinan@gmail.com>
2024-03-20 22:03:06 +0100

    Initial commit

---
+++ src/main/java/net/vanillemods/imaginarynoteblock/ImaginaryNoteblockClient.java
@@ -0,0 +1,10 @@
+package net.vanillemods.imaginarynoteblock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImaginaryNoteblockClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
